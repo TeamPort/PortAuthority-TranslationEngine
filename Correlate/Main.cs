db2e0daf55b34a6641f5874c923485c48fe0b329
Blake W. Ford <blakewford@gmail.com>
2021-01-25 23:01:37 -0600

    Add fractional values

--- Correlate/Main.cs
+++ Correlate/Main.cs
@@ -30,11 +30,22 @@ public class Correlate
             {
                 output.Add(ngrams[0], new ArrayList());
             }
-            output[ngrams[0]].Add(new Tuple<String, Int32>(ngrams[1], highest));
+            output[ngrams[0]].Add(new Tuple<String, float>(ngrams[1], highest));
             highest = 0;
         }
 
         // TODO Update values from strict counts to percentages
+        Dictionary<String, float> totals = new Dictionary<String, float>();
+        foreach(KeyValuePair<String,ArrayList> entry in output)
+        {
+            totals.Add(entry.Key, 0);
+            Int32 count = entry.Value.Count;
+            for(Int32 i = 0; i < count; i++)
+            {
+                Tuple<String, float> sub = entry.Value[i] as Tuple<String, float>;
+                totals[entry.Key] += sub.Item2;
+            }
+        }
 
         Console.WriteLine("{");
         foreach(KeyValuePair<String,ArrayList> entry in output)
@@ -43,11 +54,11 @@ public class Correlate
             Int32 count = entry.Value.Count;
             for(Int32 i = 0; i < count-1; i++)
             {
-                Tuple<String, Int32> sub = entry.Value[i] as Tuple<String, Int32>;
-                Console.WriteLine("    \"" + sub.Item1 + "\":" + sub.Item2 + ",");
+                Tuple<String, float> sub = entry.Value[i] as Tuple<String, float>;
+                Console.WriteLine("    \"" + sub.Item1 + "\":" + (sub.Item2/totals[entry.Key]).ToString("0.00") + ",");
             }
-            Tuple<String, Int32> final = entry.Value[count-1] as Tuple<String, Int32>;
-            Console.WriteLine("    \"" + final.Item1 + "\":" + final.Item2);
+            Tuple<String, float> final = entry.Value[count-1] as Tuple<String, float>;
+            Console.WriteLine("    \"" + final.Item1 + "\":" + (final.Item2/totals[entry.Key]).ToString("0.00"));
             Console.WriteLine("  },"); // TODO Last entry should not have a comma
         }
         Console.WriteLine("}");
@@ -55,6 +66,8 @@ public class Correlate
 
     private static Dictionary<String, Int32> ngrams(String file1, String file2, Int32 grams)
     {
+        // TODO handle unigrams by distributing the most common instructions, like bivec
+
         var found = new Dictionary<String, Int32>();
         String[] lines1 = File.ReadAllLines(file1);
         String[] lines2 = File.ReadAllLines(file2);
